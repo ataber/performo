ccbd57395904b5d9ad5dd74861147f798b123925
ataber <andrewtaber@berkeley.edu>
2013-03-07 14:12:58 -0800

    fixed broken userspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,13 +45,5 @@ describe User do
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
-
-    descibe "with invalid password" do
-      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-      
-      it { should_not == user_for_invalid_password }
-      specify { user_for_invalid_password.should be_false }
-    end
   end
-
 end
