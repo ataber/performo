9b71343b8cb6fb7fa75c275ab58d22be27e755fd
ataber <andrewtaber@berkeley.edu>
2013-03-16 15:18:44 -0700

    completed user signup feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Performo::Application.routes.draw do
 
   match '/signup', to: 'users#new'
-  #resources :performers
   
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
