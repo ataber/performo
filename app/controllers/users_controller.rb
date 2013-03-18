44f846ad2697e7063cce31da6f92238725d4e2c0
ataber <andrewtaber@berkeley.edu>
2013-03-18 15:48:06 -0700

    improved authorization security, fixed broken user tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
 
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def edit 
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -23,4 +39,18 @@ class UsersController < ApplicationController
     sign_out
     redirect_to root_url
   end
+
+  private 
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @user = User.find_by_id(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
