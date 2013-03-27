10f4bbdd9dba4eeb6dec9bebbddbb0988c50abc5
ataber <andrewtaber@berkeley.edu>
2013-03-27 16:07:07 -0700

    fixed some styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @performers = @user.performers
+    @performers = @user.performers.paginate(page: params[:page])
   end
 
   def new
